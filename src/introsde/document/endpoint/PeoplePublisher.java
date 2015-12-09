cd3b42cf4c6cb21b93fd656c2b23e601aa759fb8
matteogabburo <matteogabburo@gmail.com>
2015-12-09 14:46:17 +0100

    try

--- src/introsde/document/endpoint/PeoplePublisher.java
+++ src/introsde/document/endpoint/PeoplePublisher.java
@@ -1,31 +1,30 @@
 package introsde.document.endpoint;
+import introsde.document.ws.PeopleImpl;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.io.IOException;
 
-import introsde.document.ws.PeopleImplementation;
-
-import javax.xml.ws.spi.Provider;
 import javax.xml.ws.Endpoint;
 
 public class PeoplePublisher {
-    public static String SERVER_URL = "http://localhost";
-    public static String PORT = "6902";
-    public static String BASE_URL = "/ws/people";
+    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
+        String PROTOCOL = "http://";
+        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+        if (HOSTNAME.equals("127.0.0.1"))
+        {
+            HOSTNAME = "localhost";
+        }
+        String PORT = "6902";
+        String BASE_URL = "/ws/people";
 
-    public static String getEndpointURL() {
-        return SERVER_URL+":"+PORT+BASE_URL;
-    }
-
-    public static void main(String[] args) {
-
-        /*System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
-        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
-        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
-        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");*/
-
-
-        String endpointUrl = getEndpointURL();
+        if (String.valueOf(System.getenv("PORT")) != "null"){
+            PORT=String.valueOf(System.getenv("PORT"));
+        }
+        
+        String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
         System.out.println("Starting People Service...");
-        System.out.println("--> Published at = "+endpointUrl);
+        System.out.println("--> Published. Check out "+endpointUrl+"?wsdl");
         Endpoint.publish(endpointUrl, new PeopleImplementation());
     }
 }
-
